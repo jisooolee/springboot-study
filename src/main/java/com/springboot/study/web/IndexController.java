21a9161d3fb03a105d6b4418e4af0f85e66d7c6a
book0256@naver.com <book0256@naver.com>
2023-03-30 04:27:13 +0900

    5장 구글 로그인 완성

--- src/main/java/com/springboot/study/web/IndexController.java
+++ src/main/java/com/springboot/study/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.springboot.study.web;
 
+import com.springboot.study.config.auth.LoginUser;
+import com.springboot.study.config.auth.dto.SessionUser;
 import com.springboot.study.service.posts.PostsService;
 import com.springboot.study.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.servlet.http.HttpSession;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
