506ea3802c451ebc8aa720115cbf2992c7409c57
book0256@naver.com <book0256@naver.com>
2023-05-04 08:45:37 +0900

    profilecontroller 추가

---
+++ src/test/java/com/springboot/study/web/ProfileControllerUnitTest.java
@@ -0,0 +1,24 @@
+package com.springboot.study.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다(){
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
